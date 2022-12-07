41dd6c014e81ed492de0285031e978024b62eac0
user1 <user1@che>
2022-12-07 04:55:05 +0000

    fix code

--- app.py
+++ app.py
@@ -4,55 +4,32 @@ from flask import Flask, jsonify, request
 from flask_restful import Api, Resource
 from prometheus_flask_exporter import PrometheusMetrics
 
-#from model.train import train_model
 
 app = Flask(__name__)
 api = Api(app)
 metrics = PrometheusMetrics(app, group_by='endpoint')
 
-if not os.path.isfile('credit-card-fraud-model.model'):
-    print('model not found!!')
-    #train_model()
-
 loaded_model = joblib.load('credit-card-fraud-model.model')
 
-@app.route('/predict')
-@metrics.counter('predict', 'Number of prediction',
+class predict(Resource):
+    @staticmethod
+    @metrics.counter('predict', 'Number of prediction',
          labels={'result': lambda: request.predicted_class})
-def predict():
-    posted_data = request.get_json()
-    transaction_info = posted_data['transaction_detail']
+    def post():
+        posted_data = request.get_json()
+        transaction_info = posted_data['transaction_detail']
 
-    prediction = loaded_model.predict([transaction_info])[0]
+        prediction = loaded_model.predict([transaction_info])[0]
+        if prediction == 0:
+            request.predicted_class = 'normal'
+        else:
+            request.predicted_class = 'fraud'
 
-    if prediction == 0:
-        request.predicted_class = 'normal'
-    else:
-        request.predicted_class = 'fraud'
+        return jsonify({
+            'Prediction': request.predicted_class
+        })
 
-    return jsonify({
-        'Prediction': request.predicted_class
-    })
-
-
-# class MakePrediction(Resource):
-#     @staticmethod
-#     def post():
-#         posted_data = request.get_json()
-#         transaction_info = posted_data['transaction_detail']
-
-#         prediction = loaded_model.predict([transaction_info])[0]
-
-#         if prediction == 0:
-#             predicted_class = 'Normal Transaction 1'
-#         else:
-#             predicted_class = 'Fraud Transaction'
-
-#         return jsonify({
-#             'Prediction': predicted_class
-#         })
-
-# api.add_resource(MakePrediction, '/predict')
+api.add_resource(predict, '/predict')
 
 
 if __name__ == '__main__':
